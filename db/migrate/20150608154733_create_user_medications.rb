cd06c1d53ccfcaba3d34ad3d73570eff70eff0ce
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-08 11:50:17 -0400

    Add user_medication model and migration

---
+++ db/migrate/20150608154733_create_user_medications.rb
@@ -0,0 +1,13 @@
+class CreateUserMedications < ActiveRecord::Migration
+  def change
+    create_table :user_medications do |t|
+      t.references :user, null: false
+      t.references :medication, null: false
+
+      t.string :notes
+      t.integer :times_per_day
+
+      t.timestamps null: false
+    end
+  end
+end
