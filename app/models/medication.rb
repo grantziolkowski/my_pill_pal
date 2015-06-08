5206f3959661e6d13f1a058b8fbf2e9f038b9064
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-08 11:45:10 -0400

    Add Medication model with users, user_medications relations

---
+++ app/models/medication.rb
@@ -0,0 +1,5 @@
+class Medication < ActiveRecord::Base
+  has_many: :user_medications
+  has_many: :users, through: :user_medications
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  has_many: :medications
+  has_many: :user_medications
+  has_many: :medications, through: :user_medications
   has_many: :posts, foreign_key: :author_id
   has_many: :comments, foreign_key: :commenter_id
   has_many: :user_pharmacies
