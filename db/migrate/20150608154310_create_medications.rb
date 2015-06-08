ace044caf77f3e43a81f23583bd099c9a5011ea0
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-08 11:47:22 -0400

    Add medications migration

--- db/migrate/20150608154310_create_medications.rb
+++ db/migrate/20150608154310_create_medications.rb
@@ -1,7 +1,9 @@
 class CreateMedications < ActiveRecord::Migration
   def change
     create_table :medications do |t|
-
+      t.string :name, null: false
+      t.integer :dosage
+      t.string :url
       t.timestamps null: false
     end
   end
