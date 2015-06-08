1a9b709dcf7d2bd4da75f5226e588ab732386335
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-08 12:30:07 -0400

    Add sessions create, destroy methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:notice] = "Bad Username or Password"
+      redirect_to root_path
+    end
   end
 
   def destroy
+    session.clear
   end
 end
