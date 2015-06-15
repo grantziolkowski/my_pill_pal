01d33a36a70aaae19d64179568d7157a855b88d8
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-15 11:12:01 -0400

    Render html response to show view from users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,15 @@ class UsersController < ApplicationController
 
   def show
     if current_user
+      @user = current_user
       @medications = []
-      ums = UserMedication.includes(:medication).where(user: current_user)
+      ums = UserMedication.includes(:medication).where(user: @user)
       ums.map do |user_med|
         @medications << user_med.medication
       end
       respond_to do |format|
-          format.html { render :index }
-          format.json { render json: current_user, methods: :medications}
+          format.html { render :show }
+          format.json { render json: @user, methods: :medications}
       end
     else
       redirect_to root_path
