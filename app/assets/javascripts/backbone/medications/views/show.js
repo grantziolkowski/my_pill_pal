3942dca3b28ac3ce5e54c544bf51fe6da2df6869
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-30 18:21:37 -0400

    Correctly show colored pill on calendar after marking color in model creation

--- app/assets/javascripts/backbone/medications/views/show.js
+++ app/assets/javascripts/backbone/medications/views/show.js
@@ -1,15 +1,21 @@
 PillPal.Views.Medication = Backbone.View.extend({
+  initialize: function() {
+
+   },
   template: JST["backbone/medications/templates/show"],
   render: function () {
     this.$el.append(this.template(this.model.toJSON()));
-    this.$el.find("a.button").css({"background-color": this.model.toJSON().color})
+    this.color();
     return this;
   },
   renderCal: function() {
     this.$el = $("#pill_bins").find("[data-day='" + this.model.toJSON().day + "']")
     this.$el.append(this.template(this.model.toJSON()))
     this.$el.find("a.button").removeClass("small").addClass("tiny")
+    this.color()
     return this;
+  },
+  color: function () {
+    this.$el.find("a.button").css({"background-color": this.model.toJSON().color})
   }
-
 })
