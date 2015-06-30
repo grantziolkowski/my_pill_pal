0299bf94a5e8fd572547de8e5cff57228048387a
grantziolkowski <grant.ziolkowski@gmail.com>
2015-06-30 15:34:05 -0400

    Link get started button to signup panel of signin modal

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
   $('#modal').on('click', function(){
     $('a.item').remove()
   })
+  $('#get_started').on('click', function(){
+    $('a[href="#signup"]').click()
+  })
+
 });
