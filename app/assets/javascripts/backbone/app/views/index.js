fbc44759b7f702768059d81b5b5ac34d1d09e9ac
grantziolkowski <grant.ziolkowski@gmail.com>
2015-07-06 15:08:36 -0400

    Forum page renders without reloading using router.navigate trigger:true

--- app/assets/javascripts/backbone/app/views/index.js
+++ app/assets/javascripts/backbone/app/views/index.js
@@ -1,11 +1,18 @@
 PillPal.Views.Index = Backbone.View.extend({
   template: JST["backbone/app/templates/_index_navbar"],
   events: {
-    "click #profile_link": "profile"
+    "click #forum_link": "forum",
+    "click #demo_link": "profile",
+    "click #profile_link": "demo",
+    "click .signin_out_link": "handleSignInOut"
   },
-  profile: function() {
-    event.preventDefault();
+  profile: function(e) {
+    e.preventDefault();
     window.router.navigate("profile", {trigger: true});
+  },
+  forum: function(e) {
+    e.preventDefault();
+    window.router.navigate("forum", {trigger: true});
   },
   render: function() {
     this.$el.prepend(this.template);
