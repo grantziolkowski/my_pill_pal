2bd6e1664d81c2401c1fc599ccb422a2191d4429
grantziolkowski <grant.ziolkowski@gmail.com>
2015-07-07 11:20:15 -0400

    Fix foundation bug for triggering orbit slider without page reload

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -1,13 +1,23 @@
-$(document).foundation({
-  orbit: {
-    animation: 'slide',
-    timer_speed: 2000,
-    pause_on_hover: true,
-    animation_speed: 1000,
-    navigation_arrows: false,
-    bullets: false
-  }
-});
+ $(document).ready(function() {
+   $('body').on('click', '#home_link', function() {
+      setTimeout(function(){
+         $(window).trigger('resize');
+      })
+
+     $(document).foundation({
+        orbit: {
+          animation: 'slide',
+          timer_speed: 2000,
+          pause_on_hover: true,
+          animation_speed: 1000,
+          navigation_arrows: false,
+          bullets: false
+        }
+      })
+    })
+   $('#home_link').click()
+  })
+ //fix Foundation bug to trigger orbit when page is rendered without reload
 
 
 
