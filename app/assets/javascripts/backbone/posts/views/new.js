e82b40d5e1ec50cde2dadb5497342f7bca91ae5a
grantziolkowski <grant.ziolkowski@gmail.com>
2015-07-20 15:21:36 -0400

    Change content textarea to input in post form

--- app/assets/javascripts/backbone/posts/views/new.js
+++ app/assets/javascripts/backbone/posts/views/new.js
@@ -4,8 +4,13 @@ PillPal.Views.PostForm = Backbone.View.extend({
     'click .button': 'createPost'
   },
   createPost: function(e) {
+    var $form = $(event.target)
+  console.log($(this.$el.find('input[name]')))
+    this.$el.find('input[name]').each(function() {
+          // console.log(this.name)
+          // console.log(this.value);
+            })
     e.preventDefault()
-    console.log("in")
   },
   render: function() {
     this.$el.append(this.template)
