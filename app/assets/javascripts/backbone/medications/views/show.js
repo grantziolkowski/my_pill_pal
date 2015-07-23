96e449a150fcd9468cf914cad40845239a27d012
grantziolkowski <grant.ziolkowski@gmail.com>
2015-07-23 17:12:23 -0400

    Toggle med label on mouseover of pill

--- app/assets/javascripts/backbone/medications/views/show.js
+++ app/assets/javascripts/backbone/medications/views/show.js
@@ -1,8 +1,12 @@
 PillPal.Views.Medication = Backbone.View.extend({
   template: JST["backbone/medications/templates/show"],
+  events: {
+    'mouseover .med_buttons': 'showLabel'
+  },
   render: function () {
     this.$el.append(this.template(this.model.toJSON()));
     this.color();
+    // this.delegateEvents();
     return this;
   },
   renderCal: function() {
@@ -10,9 +14,14 @@ PillPal.Views.Medication = Backbone.View.extend({
     this.$el.append(this.template(this.model.toJSON()))
     this.$el.find("a.button").removeClass("small").addClass("tiny")
     this.color()
+    this.delegateEvents();
     return this;
   },
   color: function () {
     this.$el.find("a.button").css({"background-color": this.model.toJSON().color})
+  },
+  showLabel: function() {
+   var label = this.$el.children().find("[data-id='" + this.model.toJSON().id + "']")
+   label.toggle()
   }
 })
